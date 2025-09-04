a72f517438ea37e85b85ee7e3743553a80f02f97
Heee_oh <siai3647@gmail.com>
2025-09-04 19:04:07 +0900

    문법 오류 수정

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -54,6 +54,7 @@ final router = GoRouter(
     GoRoute(
       path: '/focus-v2',
       builder: (context, state) => FocusTimeScreenV2(),
+    ),
     GoRoute(
       path: '/opensource',
       builder: (context, state) => OpenSourceInfoScreen(),
