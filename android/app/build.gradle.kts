ec8fcca1561e5adb5a51a85d9c571b6b027538b9
Heee_oh <siai3647@gmail.com>
2025-11-03 16:38:50 +0900

    캘린더 버그 수정2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (envFile.exists()) {
 
 android {
     namespace = "com.zypt.focused_study_time_tracker"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -39,9 +39,9 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 22
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
 
         val kakaoKey = dotenv["KAKAO_NATIVE_APP_KEY"] as String?
         if (kakaoKey == null) {
